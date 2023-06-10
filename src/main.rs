496135aff480d08e865799c6dbf72629fa6c9d78
Affan Shahid <affan.shahid.94@gmail.com>
2023-06-10 19:11:30 +0500

    feat: add player plugin

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,21 @@
 mod platforms;
+mod player;
 
-use bevy::{prelude::*, sprite::MaterialMesh2dBundle, window::WindowResolution};
+use bevy::{prelude::*, window::WindowResolution};
 use bevy_rapier2d::prelude::*;
 use platforms::PlatformsPlugin;
+use player::PlayerPlugin;
 
 const WINDOW_WIDTH: f32 = 1024.0;
 const WINDOW_HEIGHT: f32 = 720.0;
 
 pub const WINDOW_BOTTOM_Y: f32 = WINDOW_HEIGHT / -2.0;
-const WINDOW_LEFT_X: f32 = WINDOW_WIDTH / -2.0;
+pub const WINDOW_LEFT_X: f32 = WINDOW_WIDTH / -2.0;
 
 const FLOOR_THICKNESS: f32 = 10.0;
 
 const COLOR_BACKGROUND: Color = Color::rgb(0.13, 0.13, 0.23);
-const COLOR_PLAYER: Color = Color::rgb(0.60, 0.55, 0.60);
 const COLOR_FLOOR: Color = Color::rgb(0.45, 0.55, 0.66);
-
-const PLAYER_VELOCITY_X: f32 = 400.0;
-const PLAYER_VELOCITY_Y: f32 = 850.0;
-
-const MAX_JUMP_HEIGHT: f32 = 230.0;
 
 fn main() {
     App::new()
@@ -36,35 +32,13 @@ fn main() {
         .add_plugin(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(200.0))
         .add_plugin(RapierDebugRenderPlugin::default())
         .add_plugin(PlatformsPlugin)
+        .add_plugin(PlayerPlugin)
         .add_startup_system(setup)
-        .add_system(movement)
-        .add_system(jump)
-        .add_system(rise)
-        .add_system(fall)
         .run();
 }
 
-fn setup(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
-) {
+fn setup(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
-
-    commands
-        .spawn(MaterialMesh2dBundle {
-            mesh: meshes.add(shape::Circle::default().into()).into(),
-            material: materials.add(ColorMaterial::from(COLOR_PLAYER)),
-            transform: Transform {
-                translation: Vec3::new(WINDOW_LEFT_X + 100.0, WINDOW_BOTTOM_Y + 30.0, 0.0),
-                scale: Vec3::new(30.0, 30.0, 1.0),
-                ..Default::default()
-            },
-            ..default()
-        })
-        .insert(RigidBody::KinematicPositionBased)
-        .insert(Collider::ball(0.5))
-        .insert(KinematicCharacterController::default());
 
     commands
         .spawn(SpriteBundle {
@@ -81,89 +55,4 @@ fn setup(
         })
         .insert(RigidBody::Fixed)
         .insert(Collider::cuboid(0.5, 0.5));
-}
-
-fn movement(
-    input: Res<Input<KeyCode>>,
-    time: Res<Time>,
-    mut query: Query<&mut KinematicCharacterController>,
-) {
-    let mut player = query.single_mut();
-
-    let mut movement = 0.0;
-
-    if input.pressed(KeyCode::Right) {
-        movement += time.delta_seconds() * PLAYER_VELOCITY_X;
-    }
-
-    if input.pressed(KeyCode::Left) {
-        movement += time.delta_seconds() * PLAYER_VELOCITY_X * -1.0;
-    }
-
-    match player.translation {
-        Some(vec) => player.translation = Some(Vec2::new(movement, vec.y)),
-        None => player.translation = Some(Vec2::new(movement, 0.0)),
-    }
-}
-
-#[derive(Component)]
-struct Jump(f32);
-
-fn jump(
-    input: Res<Input<KeyCode>>,
-    mut commands: Commands,
-    query: Query<
-        (Entity, &KinematicCharacterControllerOutput),
-        (With<KinematicCharacterController>, Without<Jump>),
-    >,
-) {
-    if query.is_empty() {
-        return;
-    }
-
-    let (player, output) = query.single();
-
-    if input.pressed(KeyCode::Up) && output.grounded {
-        commands.entity(player).insert(Jump(0.0));
-    }
-}
-
-fn rise(
-    mut commands: Commands,
-    time: Res<Time>,
-    mut query: Query<(Entity, &mut KinematicCharacterController, &mut Jump)>,
-) {
-    if query.is_empty() {
-        return;
-    }
-
-    let (entity, mut player, mut jump) = query.single_mut();
-
-    let mut movement = time.delta().as_secs_f32() * PLAYER_VELOCITY_Y;
-
-    if movement + jump.0 >= MAX_JUMP_HEIGHT {
-        movement = MAX_JUMP_HEIGHT - jump.0;
-        commands.entity(entity).remove::<Jump>();
-    }
-
-    jump.0 += movement;
-
-    match player.translation {
-        Some(vec) => player.translation = Some(Vec2::new(vec.x, movement)),
-        None => player.translation = Some(Vec2::new(0.0, movement)),
-    }
-}
-
-fn fall(time: Res<Time>, mut query: Query<&mut KinematicCharacterController, Without<Jump>>) {
-    if query.is_empty() {
-        return;
-    }
-
-    let mut player = query.single_mut();
-    let movement = time.delta().as_secs_f32() * (PLAYER_VELOCITY_Y / 1.5) * -1.0;
-
-    match player.translation {
-        Some(vec) => player.translation = Some(Vec2::new(vec.x, movement)),
-        None => player.translation = Some(Vec2::new(0.0, movement)),
-    }
 }
---
+++ src/player.rs
@@ -0,0 +1,131 @@
+use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
+use bevy_rapier2d::prelude::*;
+
+use crate::{WINDOW_BOTTOM_Y, WINDOW_LEFT_X};
+
+const COLOR_PLAYER: Color = Color::rgb(0.60, 0.55, 0.60);
+
+const PLAYER_VELOCITY_X: f32 = 400.0;
+const PLAYER_VELOCITY_Y: f32 = 850.0;
+
+const MAX_JUMP_HEIGHT: f32 = 230.0;
+
+pub struct PlayerPlugin;
+
+impl Plugin for PlayerPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_startup_system(setup)
+            .add_system(movement)
+            .add_system(jump)
+            .add_system(rise)
+            .add_system(fall);
+    }
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    commands.spawn(Camera2dBundle::default());
+
+    commands
+        .spawn(MaterialMesh2dBundle {
+            mesh: meshes.add(shape::Circle::default().into()).into(),
+            material: materials.add(ColorMaterial::from(COLOR_PLAYER)),
+            transform: Transform {
+                translation: Vec3::new(WINDOW_LEFT_X + 100.0, WINDOW_BOTTOM_Y + 30.0, 0.0),
+                scale: Vec3::new(30.0, 30.0, 1.0),
+                ..Default::default()
+            },
+            ..default()
+        })
+        .insert(RigidBody::KinematicPositionBased)
+        .insert(Collider::ball(0.5))
+        .insert(KinematicCharacterController::default());
+}
+
+fn movement(
+    input: Res<Input<KeyCode>>,
+    time: Res<Time>,
+    mut query: Query<&mut KinematicCharacterController>,
+) {
+    let mut player = query.single_mut();
+
+    let mut movement = 0.0;
+
+    if input.pressed(KeyCode::Right) {
+        movement += time.delta_seconds() * PLAYER_VELOCITY_X;
+    }
+
+    if input.pressed(KeyCode::Left) {
+        movement += time.delta_seconds() * PLAYER_VELOCITY_X * -1.0;
+    }
+
+    match player.translation {
+        Some(vec) => player.translation = Some(Vec2::new(movement, vec.y)),
+        None => player.translation = Some(Vec2::new(movement, 0.0)),
+    }
+}
+
+#[derive(Component)]
+struct Jump(f32);
+
+fn jump(
+    input: Res<Input<KeyCode>>,
+    mut commands: Commands,
+    query: Query<
+        (Entity, &KinematicCharacterControllerOutput),
+        (With<KinematicCharacterController>, Without<Jump>),
+    >,
+) {
+    if query.is_empty() {
+        return;
+    }
+
+    let (player, output) = query.single();
+
+    if input.pressed(KeyCode::Up) && output.grounded {
+        commands.entity(player).insert(Jump(0.0));
+    }
+}
+
+fn rise(
+    mut commands: Commands,
+    time: Res<Time>,
+    mut query: Query<(Entity, &mut KinematicCharacterController, &mut Jump)>,
+) {
+    if query.is_empty() {
+        return;
+    }
+
+    let (entity, mut player, mut jump) = query.single_mut();
+
+    let mut movement = time.delta().as_secs_f32() * PLAYER_VELOCITY_Y;
+
+    if movement + jump.0 >= MAX_JUMP_HEIGHT {
+        movement = MAX_JUMP_HEIGHT - jump.0;
+        commands.entity(entity).remove::<Jump>();
+    }
+
+    jump.0 += movement;
+
+    match player.translation {
+        Some(vec) => player.translation = Some(Vec2::new(vec.x, movement)),
+        None => player.translation = Some(Vec2::new(0.0, movement)),
+    }
+}
+
+fn fall(time: Res<Time>, mut query: Query<&mut KinematicCharacterController, Without<Jump>>) {
+    if query.is_empty() {
+        return;
+    }
+
+    let mut player = query.single_mut();
+    let movement = time.delta().as_secs_f32() * (PLAYER_VELOCITY_Y / 1.5) * -1.0;
+
+    match player.translation {
+        Some(vec) => player.translation = Some(Vec2::new(vec.x, movement)),
+        None => player.translation = Some(Vec2::new(0.0, movement)),
+    }
+}
